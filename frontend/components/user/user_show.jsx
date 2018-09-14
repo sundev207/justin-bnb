700c530352b6e08ea3175dbb8b0efe279922d677
sundev <sundev207@outlook.com>
2018-09-13 18:26:58 -0700

    Save the user and the homes into the state so that component refresh happens when state changes

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -4,28 +4,38 @@ class User extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      homes: null,
+      user: null,
+      reservedHomes: []
     };
   }
 
   componentDidMount() {
-    this.props.fetchHomes();
-    this.props.fetchUser(this.props.user.id);
+    this.props.fetchHomes()
+      .then(this.setState({ homes: this.props.homes}));
+
+    this.props.fetchUser(this.props.user.id)
+      .then(this.setState({user: this.props.user}));
   }
 
   render() {
-    if (!this.props.user.reservations) return null;
+    if (!this.state.user || !this.state.homes) {
+      return null;
+    }
+
+    console.log(this.state.user, this.state.homes);
     let { user, homes } = this.props;
 
-    let reservedHomes = [];
-    user.reservations.forEach(reservation => {
-      reservedHomes.push(homes.find(home => home.id === reservation.home_id))
-    });
-
+    // let userHomes = [];
+    // user.reservations.forEach(reservation => {
+    //   userHomes.push(homes.find(home => home.id === reservation.home_id));
+    // });
 
     return(
       <div>
         <ul>
-          {reservedHomes.forEach(home => <li> { console.log("what the hell...") } </li>)}
+          {this.state.reservedHomes.forEach(home => <li> {home.name} </li>)}
+          <li>What the hell</li>
         </ul>
       </div>
     );
